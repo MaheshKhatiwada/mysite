3a094835973611b68c3b3101f78c1e3c5906606e
MaheshKhatiwada <maheshkhatiwada17@gmail.com>
2021-08-11 09:57:46 +0545

    added social icons

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
@@ -7,6 +8,15 @@ const Home = () => {
       <h3 className=" text-secondary intro1">Hi, It's me</h3>
       <h1 className="intro2">Mahesh Khatiwada</h1>
       <h3 className="text-secondary intro1">Computer Engineer</h3>
+      </div>
+      <div className="intro-icons">
+        <ul>
+          <li><Link to ={{pathname:"https://github.com/maheshkhatiwada/"}} target="_blank"><i className="fab fa-github"></i></Link></li>
+          <li><Link to ={{pathname:"https://www.facebook.com/mahesh.khatiwada1/"}} target="_blank"><i class="fab fa-facebook"></i></Link></li>
+          <li><Link to ={{pathname:"https://www.instagram.com/mahesh.khatiwada"}}target="_blank"><i className="fab fa-instagram"></i></Link></li>
+
+
+        </ul>
       </div>
     </div>
   )
