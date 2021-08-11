f01daf35b2f8e3b59bd5c54bb26b8dbcbc2e632d
MaheshKhatiwada <maheshkhatiwada17@gmail.com>
2021-08-11 08:20:02 +0545

    added background image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
 const Home = () => {
-    return (
-        <div>
-            Home Component
-        </div>
-    )
+  return (
+    <div className="intro">
+      <div className="intro-content">
+      <h3 className=" text-secondary intro1">Hi, It's me</h3>
+      <h1 className="intro2">Mahesh Khatiwada</h1>
+      <h3 className="text-secondary intro1">Computer Engineer</h3>
+      </div>
+    </div>
+  )
 }
 
 export default Home
+
+
